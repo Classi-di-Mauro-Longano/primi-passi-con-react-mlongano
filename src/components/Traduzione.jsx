388ea81df7d4d64cfa56371878d66736674caf68
Mauro Longano <mauro@oruam.org>
2023-10-30 00:27:03 +0100

    Aggiungi la funzione debounce per limitare le chiamate

--- src/components/Traduzione.jsx
+++ src/components/Traduzione.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { findItemByKeyValue } from '../utils/utils';
+import { findItemByKeyValue, debounce } from '../utils/utils';
 import { lingue } from '../data/lingue';
 
 import { useState, useEffect } from 'react';
@@ -9,34 +9,8 @@ export function Traduzione({ lingua, testo }) {
   const API_KEY = import.meta.env.VITE_GOOGLE_TRANSLATE_API_KEY;
 
   useEffect(() => {
-
-    const fetchTraduzione = async () => {
-      setTraduzione(null);
-      if (!testo || !lingua || !API_KEY || !lingua.codice) {
-        return;
-      }
-
-      try {
-        const response = await fetch(
-          `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}&q=${testo}&target=${lingua.codice}`,
-        );
-        const data = await response.json();
-        setTraduzione(data.data.translations[0].translatedText);
-        const linguaIniziale = findItemByKeyValue("codice",data.data.translations[0].detectedSourceLanguage,lingue);
-        if (typeof linguaIniziale === 'object' && linguaIniziale.nome !== undefined ) {
-          setlinguaIniziale(linguaIniziale.nome);
-          return;
-        }
-        setlinguaIniziale(linguaIniziale);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchTraduzione();
-
+    traduci(testo, lingua, API_KEY, setTraduzione, setlinguaIniziale);
   }, [lingua, testo, API_KEY]);
-
 
   return (
     <>
@@ -53,6 +27,33 @@ export function Traduzione({ lingua, testo }) {
     </>
   );
 }
+
+const traduci = debounce(
+  async (testo, lingua, API_KEY, setTraduzione, setlinguaIniziale) => {
+      setTraduzione(null);
+      if (!testo || !lingua || !API_KEY || !lingua.codice) {
+        return;
+      }
+
+    try {
+      const response = await fetch(
+        `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}&q=${testo}&target=${lingua.codice}`,
+      );
+      const data = await response.json();
+        setTraduzione(data.data.translations[0].translatedText);
+        const linguaIniziale = findItemByKeyValue("codice",data.data.translations[0].detectedSourceLanguage,lingue);
+        if (typeof linguaIniziale === 'object' && linguaIniziale.nome !== undefined ) {
+          setlinguaIniziale(linguaIniziale.nome);
+          return;
+        }
+        setlinguaIniziale(linguaIniziale);
+      return data.data.translations[0].translatedText;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+)
+
 
 Traduzione.propTypes = {
   lingua: PropTypes.object.isRequired,
