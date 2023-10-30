ccfbff28ab1032c84ceead8b9d1ee74afb843e0d
mlongano <mauro.longano@marconirovereto.it>
2023-10-30 23:35:39 +0100

    Crea hello world in React con un componente senza JSX

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function Helloworld() {
+  return React.createElement('h1', null, 'Hello, world!');
+}
+
 ReactDOM.render(
-  React.createElement('h1', null, 'Hello, world!'),
+  React.createElement(Helloworld, null),
   document.getElementById('uno')
 );
 
